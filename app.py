6c0e9bfe00a2f6eb471f3375b2a8c3eea5797b3b
Omar Khashoggi <omar.w.kh@gmail.com>
2017-05-02 01:46:11 +0000

    debug off

--- app.py
+++ app.py
@@ -32,4 +32,4 @@ def do():
 if __name__ == '__main__':
 	port = int(os.environ.get('PORT', 8080))
 	host = os.environ.get('IP', '0.0.0.0') # port and host are to run on cloud9, remove if running locally
-	app.run(host=host, port=port, debug=True)
+	app.run(host=host, port=port, debug=False)
